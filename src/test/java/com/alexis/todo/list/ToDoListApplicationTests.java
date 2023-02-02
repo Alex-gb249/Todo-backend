16e7ead6d859e2772bb0cf5958b64d1df492c168
Alexis Guzmán Bermúdez <alexis.gb249@gmail.com>
2023-02-02 15:33:59 -0500

    Starting to-do list backend project for practice

---
+++ src/test/java/com/alexis/todo/list/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexis.todo.list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
